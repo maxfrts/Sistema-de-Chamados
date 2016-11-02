bbd7acea5b78ea87ef2c9b6813b7a4fa289e0811
Cassiano <Cassiano@FamliaLeo>
2016-11-02 17:54:33 -0200

    Testes

---
+++ src/entidade/teste/EmpresaTest.java
@@ -0,0 +1,30 @@
+package entidade.teste;
+
+import entidade.Empresa;
+import junit.framework.Assert;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Cassiano
+ */
+public class EmpresaTest {
+    
+    @Test
+    public void testGetNumeroContrato() {
+        Empresa emp = new Empresa();
+        
+        emp.setNumeroContrato(3456);
+        
+        Assert.assertEquals(3457, emp.getNumeroContrato());
+    }
+    
+    @Test
+    public void testGetNomeEmpresa() {
+        Empresa emp = new Empresa();
+        emp.setNomeEmpresa("Adidas");
+        
+        Assert.assertEquals("Adidas", emp.getNomeEmpresa());
+    }
+}
